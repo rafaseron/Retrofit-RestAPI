a917292f074f4698d2eb8604bf7c7e104b64783e
rafaseron <rafaelseron3011@gmail.com>
2024-04-11 14:40:34 -0400

    funcao para converter MovieResponse to Movie

--- app/src/main/java/br/com/alura/anyflix/network/services/MovieService.kt
+++ app/src/main/java/br/com/alura/anyflix/network/services/MovieService.kt
@@ -1,9 +1,14 @@
 package br.com.alura.anyflix.network.services
 
+import br.com.alura.anyflix.model.Movie
 import retrofit2.http.GET
 
 
 data class MovieResponse(val id: String, val title: String, val image: String, val year: Int, val plot: String, val inMyList: Boolean)
+
+fun MovieResponse.toMovie(): Movie{
+    return Movie(id = id, title = title, year = year, plot = plot, image = image, inMyList = inMyList)
+}
 
 interface MovieService {
     @GET("movies")
